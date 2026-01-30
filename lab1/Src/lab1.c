647783de0db2058af57893adcf6db23a5e67fea0
Maxwell Smith <u1397073@utah.edu>
2026-01-30 15:42:59 -0700

    Add example blinky code from the lab handout. Fixed Issue, did not copy over 2 lines that are now added

--- lab1/Src/lab1.c
+++ lab1/Src/lab1.c
@@ -19,6 +19,9 @@ int main(void)
   // Set up a configuration struct to pass to the initialization function
   GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW, GPIO_NOPULL};
 
+
+  HAL_GPIO_Init(GPIOC, &initStr);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
   while (1)
   {
     HAL_Delay(200); // Delay 200ms
