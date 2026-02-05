481beb4a008b9cfe3a7b7b4d8222f1fabf340dd2
Maxwell Smith <u1397073@utah.edu>
2026-02-04 19:28:15 -0700

    Final spacing edits etc... No actual code changes

--- lab1/Src/hal_gpio.c
+++ lab1/Src/hal_gpio.c
@@ -81,8 +81,6 @@ GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
     return GPIO_PIN_RESET;
 }
 
-
-
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
     if (PinState != GPIO_PIN_SET)
@@ -95,8 +93,6 @@ void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState 
     }
 
 }
-
-
 
 void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
